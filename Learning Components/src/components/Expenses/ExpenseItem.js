f9cc157f8662a8bd7a62664a2b7f038e3e60479b
dpakbhatt <deepakbhatt912@gmail.com>
2023-09-27 14:26:03 +0530

    useState hook implemented

--- Learning Components/src/components/Expenses/ExpenseItem.js
+++ Learning Components/src/components/Expenses/ExpenseItem.js
@@ -3,17 +3,20 @@ import "./ExpenseItem.css";
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked");
+    setTitle("Updated!");
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Clik Me!</button>
