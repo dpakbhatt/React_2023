c84a174485475a2374f3589b7fd6c54437d02bfe
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-01 13:15:23 +0530

    Removed unnecessary code

--- Learning Components/src/components/Expenses/ExpenseItem.js
+++ Learning Components/src/components/Expenses/ExpenseItem.js
@@ -3,24 +3,15 @@ import "./ExpenseItem.css";
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem reevaluated!");
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Clik Me!</button>
     </Card>
   );
 };
