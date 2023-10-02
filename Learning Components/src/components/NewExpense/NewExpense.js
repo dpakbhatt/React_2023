c08b118822ffc0a297bd32d96bb029d9d2dd2023
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-02 12:26:18 +0530

    Added form conditionally

--- Learning Components/src/components/NewExpense/NewExpense.js
+++ Learning Components/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const submitHandler = (expenseData) => {
     const expenseDataObj = {
       ...expenseData,
       id: Math.random().toString(),
     };
     props.onNewExpenseAdded(expenseDataObj);
-    return expenseDataObj;
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const cancelHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onExpenseDataSubmit={submitHandler}></ExpenseForm>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onExpenseDataSubmit={submitHandler}
+          onCancel={cancelHandler}
+        ></ExpenseForm>
+      )}
     </div>
   );
 };
