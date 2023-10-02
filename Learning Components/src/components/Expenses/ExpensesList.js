4e6ffbae15300c259e6a2519b48a6ba731968b4d
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-02 12:06:21 +0530

    Updated the implementation

--- Learning Components/src/components/Expenses/ExpensesList.js
+++ Learning Components/src/components/Expenses/ExpensesList.js
@@ -3,22 +3,22 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  let expensesContent = (
-    <p className="expenses-list__fallback">No expenses found!</p>
-  );
-
-  if (props.filteredData.length > 0) {
-    expensesContent = props.filteredData.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
+  if (props.filteredData.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
 
-  return <ul className="expenses-list">{expensesContent}</ul>;
+  return (
+    <ul className="expenses-list">
+      {props.filteredData.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default ExpensesList;
