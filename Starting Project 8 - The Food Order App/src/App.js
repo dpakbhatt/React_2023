1b3bbf3d7d7cd5b10b0854511beb16f2ef138e39
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-09 14:56:22 +0530

    Added state for handling cart visibility

--- Starting Project 8 - The Food Order App/src/App.js
+++ Starting Project 8 - The Food Order App/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartVisible(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {isCartVisible && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- Starting Project 8 - The Food Order App/src/components/Cart/Cart.js
+++ Starting Project 8 - The Food Order App/src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
     (item) => {
       return <li>{item.name}</li>;
@@ -11,14 +11,16 @@ const Cart = () => {
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onCloseCart}>
       <ul className={classes["cart-items"]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.40</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
