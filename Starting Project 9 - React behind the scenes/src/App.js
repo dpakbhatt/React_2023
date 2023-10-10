cc92e43a05d4164c3b99ecda3558924e56abc7e7
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-10 18:44:59 +0530

    Implemneted useMemo for more optimization

--- Starting Project 9 - React behind the scenes/src/App.js
+++ Starting Project 9 - React behind the scenes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -20,10 +20,12 @@ function App() {
     setAllowToggle(true);
   };
 
+  const list = useMemo(() => [1, 5, 6, 2, 9, 3, 12], []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={showParagraph} items={list} />
       <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
--- Starting Project 9 - React behind the scenes/src/components/Demo/DemoOutput.js
+++ Starting Project 9 - React behind the scenes/src/components/Demo/DemoOutput.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput RUNNING");
-  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
+  const { items } = props;
+
+  const sortedList = useMemo(() => {
+    console.log("RUNNING");
+    return items.sort((a, b) => a - b);
+  }, [items]);
+
+  return (
+    <>
+      <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>
+      {sortedList.map((item) => {
+        return <p key={item}>{item}</p>;
+      })}
+    </>
+  );
 };
 
 export default React.memo(DemoOutput);
