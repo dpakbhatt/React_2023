d233b951cd8fa2803cc0009e7f408ffc8657dac5
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-10 11:48:21 +0530

    Implemented useEffect for an animation when the items gets added to the cart

--- Starting Project 8 - The Food Order App/src/components/Layout/HeaderCartButton.js
+++ Starting Project 8 - The Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCntxt = useContext(CartContext);
+  const { items } = cartCntxt;
 
-  const numberOfItemsInCart = cartCntxt.items.reduce((numberOfItems, item) => {
+  const numberOfItemsInCart = items.reduce((numberOfItems, item) => {
     return numberOfItems + item.amount;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
