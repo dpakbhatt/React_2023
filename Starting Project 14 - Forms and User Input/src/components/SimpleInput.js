248aaa56853f2ff81437b4e8a5e8b177b8810f4c
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-12 01:34:49 +0530

    Added more changes to simpleInput.js file related to onChange and ref

--- Starting Project 14 - Forms and User Input/src/components/SimpleInput.js
+++ Starting Project 14 - Forms and User Input/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const inputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      return;
+    }
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={inputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
