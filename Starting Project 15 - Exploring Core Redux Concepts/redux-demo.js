529abd09c60a85a57e4361583a264846291fdd41
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-13 01:43:53 +0530

    Added new project to explore redux core concepts

---
+++ Starting Project 15 - Exploring Core Redux Concepts/redux-demo.js
@@ -0,0 +1,20 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+// console.log(store.getState());
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
