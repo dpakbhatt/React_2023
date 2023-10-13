53742162451c176471bbc75c746fab9e4f15e74d
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-13 11:42:57 +0530

    Implemented redux in class based comp using connect

--- Starting Project 16 - React Implementation/src/components/Counter.js
+++ Starting Project 16 - React Implementation/src/components/Counter.js
@@ -1,15 +1,79 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch, connect } from "react-redux";
+import classes from "./Counter.module.css";
+import { Component } from "react";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => {
+    return state.counter;
+  });
+
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
 
 export default Counter;
+
+// class Counter extends Component {
+//   constructor() {
+//     super();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
