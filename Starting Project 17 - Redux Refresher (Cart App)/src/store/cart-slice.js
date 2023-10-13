1d86a8e4f12c6bb0ebc857f99a1d8f21723409b8
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-13 19:19:46 +0530

    Implemented cartSlice as well and completed all the necessary steps

--- Starting Project 17 - Redux Refresher (Cart App)/src/store/cart-slice.js
+++ Starting Project 17 - Redux Refresher (Cart App)/src/store/cart-slice.js
@@ -2,14 +2,42 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialCartState = {
   items: [],
+  totalQuantity: 0,
 };
 
 const cartSlice = createSlice({
   name: "forCart",
   initialState: initialCartState,
   reducers: {
-    addToCart(state, action) {},
-    removeFromCart(state, action) {},
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        state;
+      } else {
+        state.items.push({
+          id: newItem.id,
+          name: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
