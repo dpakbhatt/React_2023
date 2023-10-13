605353977e7f9cb2847db2f9367a658a4b290b50
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-14 02:33:01 +0530

    Implemented useEffect for sending cart data to firebase after state gets updated

--- Starting Project 17 - Redux Refresher (Cart App)/src/App.js
+++ Starting Project 17 - Redux Refresher (Cart App)/src/App.js
@@ -3,11 +3,21 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isCartVisible = useSelector((state) => {
     return state.ui.isCartVisible;
   });
+
+  useEffect(() => {
+    fetch("https://testing-http-a9632-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {isCartVisible && <Cart />}
