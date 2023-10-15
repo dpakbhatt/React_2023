74c2b67d8ee6088617b7ce4b2d06dc96dd858847
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-16 00:33:22 +0530

    Added new page as the second route

--- Starting Project 18 - Building Multipage SPA with React Router/src/App.js
+++ Starting Project 18 - Building Multipage SPA with React Router/src/App.js
@@ -1,7 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Product from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <Product /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
