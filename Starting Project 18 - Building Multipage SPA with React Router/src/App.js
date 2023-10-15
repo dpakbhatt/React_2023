c0423b33ba0f3e2156decc8fc4d244759cd865d5
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-16 00:37:59 +0530

    Added another approach as well for defining routes

--- Starting Project 18 - Building Multipage SPA with React Router/src/App.js
+++ Starting Project 18 - Building Multipage SPA with React Router/src/App.js
@@ -1,6 +1,21 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Products";
+
+// WE CAN DEFINE ROUTER USING THIS APPROACH AS WELL
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<Product />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
