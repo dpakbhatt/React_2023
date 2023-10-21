d71636d493bf19132ce4faa6f8404ad79c979c21
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-22 01:15:29 +0530

    Linking between the pages implemented

--- Starting Project 23 - NextJs Project/pages/news/index.js
+++ Starting Project 23 - NextJs Project/pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/testing-1">Testing 1</Link>
+        </li>
+        <li>
+          <Link href="/news/testing-2">Testing 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
