87a9c9e4e571002e82747ce7284f0e7deeed0c0a
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-24 01:49:42 +0530

    Handler function implemented in the api route for new-meetup and sent data to mongodb

--- Starting Project 24 - NextJs Meetup Project/pages/api/new-meetup.js
+++ Starting Project 24 - NextJs Meetup Project/pages/api/new-meetup.js
@@ -1,10 +1,25 @@
 // /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
-    const data = req.body;
+    try {
+      const data = req.body;
 
-    const { title, image, address, description } = data;
+      const client = await MongoClient.connect(
+        "mongodb+srv://deepak:bholenath1atlaslatest@reactcluster.oivma2q.mongodb.net/meetups?retryWrites=true&w=majority"
+      );
+      const db = client.db();
+
+      const meetupsCollection = db.collection("meetups");
+      const result = await meetupsCollection.insertOne(data);
+
+      console.log(result);
+      client.close();
+      res.status(201).json({ message: "Meetup inserted!" });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
--- Starting Project 24 - NextJs Meetup Project/pages/new-meetup/index.js
+++ Starting Project 24 - NextJs Meetup Project/pages/new-meetup/index.js
@@ -1,8 +1,17 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function onAddMeetupHandler(enteredData) {
-    console.log(enteredData);
+  async function onAddMeetupHandler(enteredData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
 
   return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
