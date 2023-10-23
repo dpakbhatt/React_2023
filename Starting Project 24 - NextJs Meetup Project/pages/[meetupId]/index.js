5e642f16728c205de756de7133c0097e53e5bf32
dpakbhatt <deepakbhatt912@gmail.com>
2023-10-23 22:58:56 +0530

    Custom component implemented and added css module

--- Starting Project 24 - NextJs Meetup Project/pages/[meetupId]/index.js
+++ Starting Project 24 - NextJs Meetup Project/pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 function DetailsPage() {
-  return <h1>The Details Page</h1>;
+  return (
+    <MeetupDetail
+      title="First Meetup"
+      image="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHx8MA%3D%3D"
+      description="This is first meetup"
+      address="Address 1, Street No. 1, City 1"
+    />
+  );
 }
 
 export default DetailsPage;
