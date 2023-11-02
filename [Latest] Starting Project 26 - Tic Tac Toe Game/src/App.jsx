f9161d033204dd86ee524119ce013f5ed9030083
dpakbhatt <deepakbhatt912@gmail.com>
2023-11-03 00:33:32 +0530

    Game over screen added

--- [Latest] Starting Project 26 - Tic Tac Toe Game/src/App.jsx
+++ [Latest] Starting Project 26 - Tic Tac Toe Game/src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -53,6 +54,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSquareSelect(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -73,7 +76,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSquareSelect={handleSquareSelect} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
