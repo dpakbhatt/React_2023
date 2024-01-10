069777cb5df0c799439681aba8f2510cbee74c13
dpakbhatt <deepakbhatt912@gmail.com>
2024-01-11 01:50:28 +0530

    Conditional statement added to show the component accordingly

--- [Latest] Starting Project 29 - Project Management App/src/App.jsx
+++ [Latest] Starting Project 29 - Project Management App/src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar />
-        <NoProjectSelected />
-        {/* <NewProject /> */}
+        <ProjectsSidebar addProjectClicked={handleAddProject} />
+        {projectState.selectedProjectId === null ? (
+          <NewProject />
+        ) : (
+          <NoProjectSelected createNewProjectClicked={handleAddProject} />
+        )}
       </main>
     </>
   );
