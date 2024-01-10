c018ae5b534f51e817695ec51f3175f504ccbe10
dpakbhatt <deepakbhatt912@gmail.com>
2024-01-11 01:38:12 +0530

    Added NoProjectSelected component and shifted button code to a separate component so that it can be reused

--- [Latest] Starting Project 29 - Project Management App/src/App.jsx
+++ [Latest] Starting Project 29 - Project Management App/src/App.jsx
@@ -1,4 +1,5 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
     <>
       <main className="h-screen my-8 flex gap-8">
         <ProjectsSidebar />
-        <NewProject />
+        <NoProjectSelected />
+        {/* <NewProject /> */}
       </main>
     </>
   );
