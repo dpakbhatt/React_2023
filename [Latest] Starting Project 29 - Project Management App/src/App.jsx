23445e441f4e7df4b3c0a1b30e0520f8cf28b2d9
dpakbhatt <deepakbhatt912@gmail.com>
2024-01-12 02:48:00 +0530

    selectedProject now implemented

--- [Latest] Starting Project 29 - Project Management App/src/App.jsx
+++ [Latest] Starting Project 29 - Project Management App/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState, setProjectState] = useState({
@@ -39,21 +40,41 @@ function App() {
     });
   }
 
+  function handleSelectProject(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  const selectedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} />;
+
+  if (projectState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        saveClicked={handleSaveProject}
+        cancelClicked={handleCancelProject}
+      />
+    );
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected createNewProjectClicked={handleAddProject} />;
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
         <ProjectsSidebar
           addProjectClicked={handleAddProject}
           projects={projectState.projects}
+          onSelectProject={handleSelectProject}
         />
-        {projectState.selectedProjectId === null ? (
-          <NewProject
-            saveClicked={handleSaveProject}
-            cancelClicked={handleCancelProject}
-          />
-        ) : (
-          <NoProjectSelected createNewProjectClicked={handleAddProject} />
-        )}
+        {content}
       </main>
     </>
   );
