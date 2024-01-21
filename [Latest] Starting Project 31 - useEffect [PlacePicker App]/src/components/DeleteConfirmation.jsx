7ff3b5c8858005c8977ac5965715d1117116e185
dpakbhatt <deepakbhatt912@gmail.com>
2024-01-22 00:50:09 +0530

    Implemented cleanup function

--- [Latest] Starting Project 31 - useEffect [PlacePicker App]/src/components/DeleteConfirmation.jsx
+++ [Latest] Starting Project 31 - useEffect [PlacePicker App]/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, []);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
