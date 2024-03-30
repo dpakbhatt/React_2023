3a7c930824fcebe619f3616a6c492d805a5be9ba
dpakbhatt <deepakbhatt912@gmail.com>
2024-03-30 14:37:04 +0530

    Used formData to extract the data

--- [Latest] Starting Project 36 - React Forms/src/components/Signup.jsx
+++ [Latest] Starting Project 36 - React Forms/src/components/Signup.jsx
@@ -1,6 +1,15 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formDataObj = new FormData(event.target);
+    const fromdata = Object.fromEntries(formDataObj.entries());
+    const acquisitionGroup = formDataObj.getAll("acquisition");
+    fromdata.acquisition = acquisitionGroup;
+    console.log("Submitted: ", fromdata);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
