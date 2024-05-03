7007ae978498176840abe7b7e169af56ce00af41
dpakbhatt <deepakbhatt912@gmail.com>
2024-05-03 15:57:51 +0530

    Stagger children added

--- [Latest] Starting Project 40 - Animation/src/components/NewChallenge.jsx
+++ [Latest] Starting Project 40 - Animation/src/components/NewChallenge.jsx
@@ -57,7 +57,12 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul
+          id="new-challenge-images"
+          variants={{
+            visible: { transition: { staggerChildren: 0.05 } },
+          }}
+        >
           {images.map((image) => (
             <motion.li
               variants={{
@@ -65,6 +70,7 @@ export default function NewChallenge({ onDone }) {
                 visible: { opacity: 1, scale: 1 },
               }}
               exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: "spring" }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
               className={selectedImage === image ? "selected" : undefined}
@@ -72,7 +78,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
